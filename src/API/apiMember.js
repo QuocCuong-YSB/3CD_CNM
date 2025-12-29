fc00573f5afc7700295defb2daa41d56dda75556
Nguyen Van Cong <congnguyenvan522@gmail.com>
2025-12-29 21:38:38 +0700

    Feature: get & update user profile

--- src/API/apiMember.js
+++ src/API/apiMember.js
@@ -1,5 +1,36 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
-export default axios.create({
+const apiMember = axios.create({
     baseURL: `http://localhost:8000/api/member`,
 });
+apiMember.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+apiMember.interceptors.response.use(
+    (res) => res,
+    (err) => {
+        if (err.response?.status === 401) {
+            localStorage.clear();
+            toast.error('Bạn chưa đăng nhập');
+            setTimeout(() => {
+                window.location.href = '/';
+            }, 1500);
+        }
+        if (err.response?.status === 403) {
+            localStorage.clear();
+            toast.error('Bạn không có quyền truy cập');
+            setTimeout(() => {
+                window.location.href = '/';
+            }, 1500);
+        }
+
+        return Promise.reject(err);
+    },
+);
+export default apiMember;
